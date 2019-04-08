1828bce5986743a9656e4a564fae742c29b60aeb
Diego Rayo <diegofrayo@gmail.com>
2019-04-08 11:18:34 -0500

    :heavy_plus_sign: [add] utilities package

--- src/scripts.js
+++ src/scripts.js
@@ -1,53 +1,6 @@
-((window, document) => {
+((window, document, dfrz) => {
   const APP_CONFIGURATION = {
-    ENVIRONMENT:
-      window.location.href.indexOf('localhost') !== -1 ? 'development' : 'production',
-  };
-
-  const initGA = () => {
-    (function(i, s, o, g, r, a, m) {
-      i['GoogleAnalyticsObject'] = r;
-      (i[r] =
-        i[r] ||
-        function() {
-          (i[r].q = i[r].q || []).push(arguments);
-        }),
-        (i[r].l = 1 * new Date());
-      (a = s.createElement(o)), (m = s.getElementsByTagName(o)[0]);
-      a.async = 1;
-      a.src = g;
-      m.parentNode.insertBefore(a, m);
-    })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
-
-    const location = window.location.href;
-
-    ga('create', 'UA-98284306-1', 'auto', 'website', {
-      location: location,
-      page: '/',
-      title: document.title,
-    });
-    ga('set', 'location', location);
-    ga('set', 'page', '/');
-    ga('website.send', 'pageview');
-  };
-
-  const getCookie = cname => {
-    const cookieData = document.cookie.split(';');
-    const name = cname + '=';
-
-    for (let index = 0; index < cookieData.length; index += 1) {
-      let cookieChunk = cookieData[index];
-
-      while (cookieChunk.charAt(0) === ' ') {
-        cookieChunk = cookieChunk.substring(1);
-      }
-
-      if (cookieChunk.indexOf(name) === 0) {
-        return cookieChunk.substring(name.length, cookieChunk.length);
-      }
-    }
-
-    return false;
+    ENVIRONMENT: dfrz.getEnvironment(),
   };
 
   const onClickGA_Element = event => {
@@ -73,10 +26,10 @@
   };
 
   const onReadyHandler = () => {
-    const isLoggedIn = getCookie('auth');
+    const isLoggedIn = dfrz.getCookie('auth');
 
     if (APP_CONFIGURATION.ENVIRONMENT === 'production' && isLoggedIn === false) {
-      initGA();
+      dfrz.initGA();
       configureGAEvents();
     } else {
       document.getElementById('login-flag').style.display = 'block';
@@ -84,4 +37,4 @@
   };
 
   document.addEventListener('DOMContentLoaded', onReadyHandler, false);
-})(window, document);
+})(window, document, window.dfrz);
