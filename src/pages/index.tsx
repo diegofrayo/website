65d81d0364142ae5a1eae9381d345d3b3b54dc3e
Diego Rayo <diegofrayo@gmail.com>
2020-11-30 22:48:51 -0500

    :lipstick: [improve] homepage styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 
 import { Page } from "~/components/layout";
+import tw from "~/lib/twcss";
 
 function Home(): any {
   const SOCIAL_NETWORKS = [
@@ -30,14 +31,21 @@ function Home(): any {
             Hola, soy <strong>Diego Rayo</strong> 👋
           </h1>
 
-          <p className="tw-text-gray-700 tw-text-justify tw-leading-snug">
+          <p className="tw-text-gray-700 tw-text-justify tw-leading-snug tw-mb-4">
             Soy desarrollador de Software. Trabajo usualmente con JavaScript, React,
-            Next.js, TailwindCSS, Node y GraphQL. Acabo de crear este sitio web en donde
-            espero escribir acerca de mi experiencia usando las herramientas que uso día a
-            día y con algunas que quiero probar y aún no le he hecho. 🤓 <br /> Así que
-            planeo construir este sitio web con un montón de herramientas innecesarias
+            Next.js, Tailwind CSS, Node.js y GraphQL. Acabo de crear este sitio web en
+            donde quiero escribir acerca de mi experiencia usando las herramientas que uso
+            día a día y con algunas que quiero probar y aún no le he hecho. 🤓 <br /> Así
+            que planeo construir este sitio web con un montón de herramientas innecesarias
             para la simplicidad de este sitio. 🙈
           </p>
+
+          <p className="tw-bg-yellow-200 tw-border tw-border-yellow-400 tw-p-4 tw-text-center tw-block tw-text-base tw-text-yellow-700 tw-text-opacity-75 tw-rounded-sm tw-shadow-md tw-font-bold tw-mb-8">
+            Es altamente probable que nunca construya este sitio web y en una semana no me
+            vuelva a acordar de esto. 😬
+          </p>
+
+          <MainMenu></MainMenu>
         </section>
 
         <footer className="tw-text-center tw-bg-gray-800 tw-pt-2 tw-mt-4">
@@ -68,5 +76,22 @@ function SocialIcon({ icon, url }) {
     </a>
   );
 }
+
+function MainMenu() {
+  function handleItemClick() {
+    alert("Trabajo en progreso...");
+  }
+
+  return (
+    <nav className="tw-border tw-border-gray-100">
+      <ul className="tw-flex tw-flex-no-wrap">
+        <MainMenuItem onClick={handleItemClick}>🤚 Acerca de mi</MainMenuItem>
+        <MainMenuItem onClick={handleItemClick}>✍️ Blog</MainMenuItem>
+      </ul>
+    </nav>
+  );
+}
+
+const MainMenuItem = tw.li`tw-flex-1 tw-flex-shrink-0 tw-text-center tw-border tw-border-gray-200 tw-p-1 hover:tw-bg-gray-200 tw-cursor-pointer tw-transition-all tw-text-gray-800`;
 
 export default Home;
