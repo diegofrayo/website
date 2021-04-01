1c2f0763ff024e29cbdcb8747928522d0233773a
Diego Rayo <diegofrayo@gmail.com>
2021-04-01 12:57:47 -0500

    :bug: [fix] analytics issue

--- packages/website/src/utils/analytics.ts
+++ packages/website/src/utils/analytics.ts
@@ -2,7 +2,7 @@ import { isUserLoggedIn } from "./misc";
 
 export async function initAnalytics(): Promise<void> {
   if (isUserLoggedIn() === true) return;
-  const splitbee: any = await import("@splitbee/web");
+  const splitbee: any = (await import("@splitbee/web")).default;
   splitbee.init();
 }
 
