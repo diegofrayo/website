06073fac480d5de5eab074691d1af01884bbc19d
Diego Rayo <diegofrayo@gmail.com>
2021-08-09 10:32:42 -0500

    :art: [improve] playground pages config

--- packages/website/src/server/index.ts
+++ packages/website/src/server/index.ts
@@ -1,14 +1,11 @@
 import http from "~/lib/http";
 import { ASSETS_LOCAL_PATH } from "~/utils/constants";
-import { isDevelopmentEnvironment } from "~/utils/misc";
 
 export async function dataLoader({ path }: { path: string }): Promise<unknown> {
   if (process.env.NEXT_PUBLIC_IS_OWNER) {
-    if (isDevelopmentEnvironment()) {
-      const response = await http.get(`${process.env.NEXT_PUBLIC_ASSETS_SERVER_URL}${path}`);
+    const response = await http.get(`${process.env.NEXT_PUBLIC_ASSETS_SERVER_URL}${path}`);
 
-      return response.data;
-    }
+    return response.data;
   }
 
   // eslint-disable-next-line @typescript-eslint/no-var-requires
