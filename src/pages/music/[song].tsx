6a47cc396f17fa41832941e7375b7a54a7d01603
Diego Rayo <diegofrayo@gmail.com>
2021-10-21 10:21:36 -0500

    :loud_sound: [add] a few logs

--- src/pages/music/[song].tsx
+++ src/pages/music/[song].tsx
@@ -155,6 +155,8 @@ export default SongPage;
 type T_StaticPath = { params: { song: string } };
 
 export const getStaticPaths: GetStaticPaths<{ song: string }> = async function getStaticPaths() {
+  console.log("getStaticPaths", new Date());
+
   return {
     paths: (await MusicService.fetchSongsList()).reduce((result: T_StaticPath[], song: T_Song) => {
       if (song.isPublic === false) return result;
@@ -168,8 +170,11 @@ export const getStaticProps = getPageContentStaticProps<T_PageProps, { song: str
   page: [ROUTES.MUSIC, ROUTES.MUSIC_DETAILS],
   locale: "es",
   callback: async ({ params }) => {
+    console.log("getStaticProps", new Date(), "id: " + params?.song);
+
     const song = await MusicService.getSong({ id: params?.song });
     const file = await dataLoader({ path: `/pages/music/[song]/assets/${song.id}.mdx` });
+
     const songMDXContent = await renderToString(file, {
       components: MDXComponents,
       scope: {
