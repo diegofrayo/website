10981f7d635c3455b162927f84ff40454eb7a81c
Diego Rayo <diegofrayo@gmail.com>
2022-03-08 22:10:47 -0500

    :sparkles: [implement] ISR on demand

--- src/components/pages/personal/[page]/Contacts.tsx
+++ src/components/pages/personal/[page]/Contacts.tsx
@@ -1,24 +1,27 @@
 import * as React from "react";
 
 import { Page, MainLayout } from "~/components/layout";
-import { Block, InlineText, Link, List, Space, Title } from "~/components/primitive";
-import type { T_ReactElement } from "~/types";
+import { Block, Link, List, Space, Text, Title } from "~/components/primitive";
+import { useDidMount } from "~/hooks";
+import type { T_Object, T_ReactElement } from "~/types";
 import { isMobileiOS } from "~/utils/browser";
 import { isBrowser } from "~/utils/misc";
+import { generateSlug } from "~/utils/strings";
 
-function Contacts({ contacts }: { contacts: any }): T_ReactElement {
+function Contacts({ contacts }: T_ContactsProps): T_ReactElement {
   const PAGE_TITLE = "Contacts";
 
-  // function countContacts(contacts) {
-  //   return Object.values(contacts).reduce((result, contacts) => {
-  //     return result + contacts.length;
-  //   }, 0);
-  // }
+  // effects
+  useDidMount(() => {
+    console.log(countContacts(contacts.data));
+  });
 
-  //   && !contact.phone.split(" ")[1]?.startsWith("60")
-
-  // https://help.heroku.com/TXYS53YJ/why-is-my-node-js-build-failing-because-of-an-outdated-yarn-lockfile
-  // Santiago Trejos
+  // utils
+  function countContacts(contacts: T_Contacts) {
+    return Object.values(contacts).reduce((result, contacts) => {
+      return result + contacts.length;
+    }, 0);
+  }
 
   return (
     <Page
@@ -28,72 +31,35 @@ function Contacts({ contacts }: { contacts: any }): T_ReactElement {
       }}
     >
       <MainLayout title={`${PAGE_TITLE}`}>
-        {Object.entries({
-          "Amigos cercanos": contacts["Amigos cercanos"],
-          "Más contactos": contacts["Más contactos"],
-          Familia: contacts["Familia"],
-          "Otros contactos": contacts["Otros contactos"],
-          Negocios: contacts["Negocios"],
-          "Extranjeros y viajeros": contacts["Extranjeros y viajeros"],
-          Archivados: contacts["Archivados"],
-        }).map(([groupName, contacts]) => {
+        {Object.entries(
+          contacts.order.reduce((result, curr) => {
+            return {
+              [curr]: contacts.data[curr],
+              ...result,
+            };
+          }, {}),
+        ).map(([groupName, contacts]: [string, T_Contact[]]) => {
           return (
             <Block key={groupName} className="tw-mb-8 last:tw-mb-0">
               <Title is="h2">{`❏ ${groupName} [${contacts.length}]`}</Title>
               <Space size={1} />
+
               <List variant={List.variant.DEFAULT}>
                 {contacts.map((contact) => {
                   if (contact.title) {
                     return (
                       <Title
-                        key={contact.title}
+                        key={generateSlug(contact.title)}
                         is="h3"
-                        className="tw-mt-8 tw-mb-2 tw--ml-5"
+                        className="tw-mt-8 tw-mb-2 tw--ml-5 first:tw-mt-4"
                       >{`⁆ ${contact.title}`}</Title>
                     );
                   }
 
                   return (
-                    <List.Item key={contact.name} className="tw-font-bold">
-                      <InlineText>{contact.name}</InlineText> [
-                      <Block className="tw-inline-block">
-                        {typeof contact.phone === "string" && contact.phone ? (
-                          <LinkItem
-                            href={`https://${
-                              isBrowser() && isMobileiOS() ? "api" : "web"
-                            }.whatsapp.com/send?phone=${contact.phone.replace(" ", "")}`.trim()}
-                          >
-                            WHATSAPP
-                          </LinkItem>
-                        ) : null}
-                        {contact.instagram && (
-                          <LinkItem href={`https://instagram.com/${contact.instagram}`}>
-                            INSTAGRAM
-                          </LinkItem>
-                        )}
-                        {Array.isArray(contact.phone) ? (
-                          contact.phone.map((item) => {
-                            if (!item.value.includes("+57")) return null;
-
-                            return (
-                              <LinkItem
-                                key={"hppp"}
-                                href={`tel:${item.value.split(" ").slice(1).join("").trim()}`}
-                              >
-                                {`LLAMAR${item.label ? ` (${item.label})` : ""}`}
-                              </LinkItem>
-                            );
-                          })
-                        ) : contact.phone.includes("+57") ? (
-                          <LinkItem
-                            key={"hppp"}
-                            href={`tel:${contact.phone.split(" ").slice(1).join("").trim()}`}
-                          >
-                            {`LLAMAR${contact.label ? ` (${contact.label})` : ""}`}
-                          </LinkItem>
-                        ) : null}
-                      </Block>
-                      ]
+                    <List.Item key={generateSlug(contact.name)} className="tw-font-bold">
+                      <Text>{contact.name}</Text>
+                      <ContactLinks contact={contact} />
                     </List.Item>
                   );
                 })}
@@ -110,7 +76,80 @@ export default Contacts;
 
 // --- Components ---
 
-function LinkItem({ href, children }) {
+function ContactLinks({ contact }: { contact: T_Contact }) {
+  function generatePhoneLink(phone: string) {
+    return `tel:${phone.split(" ").slice(1).join("").trim()}`;
+  }
+
+  function generateWhatsAppLink(phone: string) {
+    if (phone.split(" ")[1]?.startsWith("60")) return "";
+
+    return `https://${
+      isBrowser() && isMobileiOS() ? "api" : "web"
+    }.whatsapp.com/send?phone=${phone.replace(" ", "")}`.trim();
+  }
+
+  function isPhoneNumberFromColombia(phone: string) {
+    return phone.includes("+57");
+  }
+
+  return (
+    <div className="root">
+      {Array.isArray(contact.phone) ? (
+        contact.phone.map((item) => {
+          return (
+            <ContactLinksItem
+              key={generateSlug(item.label)}
+              href={generateWhatsAppLink(item.value)}
+            >
+              {`WHATSAPP (${item.label})`}
+            </ContactLinksItem>
+          );
+        })
+      ) : contact.phone ? (
+        <ContactLinksItem href={generateWhatsAppLink(contact.phone)}>WHATSAPP</ContactLinksItem>
+      ) : null}
+
+      {contact.instagram ? (
+        <ContactLinksItem href={`https://instagram.com/${contact.instagram}`}>
+          INSTAGRAM
+        </ContactLinksItem>
+      ) : null}
+
+      {Array.isArray(contact.phone) ? (
+        contact.phone.map((item) => {
+          if (!isPhoneNumberFromColombia(item.value)) return null;
+
+          return (
+            <ContactLinksItem key={generateSlug(item.label)} href={generatePhoneLink(item.value)}>
+              {`LLAMAR (${item.label})`}
+            </ContactLinksItem>
+          );
+        })
+      ) : isPhoneNumberFromColombia(contact.phone) ? (
+        <ContactLinksItem href={generatePhoneLink(contact.phone)}>LLAMAR</ContactLinksItem>
+      ) : null}
+
+      <style jsx>{`
+        .root::before {
+          content: "[";
+          display: inline-block;
+          margin-right: 3px;
+        }
+
+        .root::after {
+          content: "]";
+          display: inline-block;
+          margin-left: 3px;
+        }
+      `}</style>
+    </div>
+  );
+}
+
+function ContactLinksItem({ href = "", children }) {
+  if (!href) return null;
+
   return (
     <div className="root tw-inline-block">
       <Link variant={Link.variant.PRIMARY} href={href} isExternalUrl>
@@ -131,3 +170,23 @@ function LinkItem({ href, children }) {
     </div>
   );
 }
+
+// --- Types ---
+
+type T_ContactsProps = {
+  contacts: {
+    order: string[];
+    data: T_Contacts;
+  };
+};
+
+type T_Contacts = T_Object<T_Contact[]>;
+
+type T_Contact = {
+  name: string;
+  phone: string | { label: string; value: string }[];
+  instagram: string;
+  group: string;
+
+  title?: string;
+};
---
+++ src/pages/api/diegofrayo.ts
@@ -0,0 +1,20 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse): Promise<void> {
+  // Check for secret to confirm this is a valid request
+  if (req.query.secret !== process.env.ISR_TOKEN) {
+    console.log("Invalid token", req.query.secret, process.env.ISR_TOKEN);
+    res.status(401).json({ message: "Invalid token" });
+
+    return;
+  }
+
+  try {
+    await res.unstable_revalidate("/personal/contacts");
+    res.json({ revalidated: true, date: new Date() });
+  } catch (err) {
+    // If there was an error, Next.js will continue
+    // to show the last successfully generated page
+    res.status(500).send("Error revalidating");
+  }
+}
