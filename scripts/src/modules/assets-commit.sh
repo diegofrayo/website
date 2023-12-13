481d4de5178c2f94969180481572063d8392ad3b
Diego Rayo <diegofrayo@gmail.com>
2023-12-12 21:24:16 -0500

    :bento: [update] assets

--- scripts/src/modules/assets-commit.sh
+++ scripts/src/modules/assets-commit.sh
@@ -1,2 +1,3 @@
+npm run script:sync-shared-lib
 git add .
 git commit -m ":bento: [update] assets" --no-verify
--- scripts/src/modules/generic-commit.sh
+++ scripts/src/modules/generic-commit.sh
@@ -1,5 +1,6 @@
 npm run build:ts
 npm run format
 npm run lint
+npm run script:sync-shared-lib
 git add .
 git commit -m ":art: [minor] changes" --no-verify
