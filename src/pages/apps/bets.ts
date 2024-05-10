f9154b45c45fc81c18fbc2b394bc0e26da0e0bfa
Diego Rayo <diegofrayo@gmail.com>
2024-05-09 22:58:46 -0500

    :arrow_up: [upgrade] next.js to 14.1.1

--- src/pages/apps/bets.ts
+++ src/pages/apps/bets.ts
@@ -4,6 +4,7 @@ import dayjs from "dayjs";
 import BetsPage from "~/modules/pages/apps/pages/BetsPage";
 import type { T_BetsPageProps } from "~/modules/pages/apps/pages/BetsPage";
 import { loadData } from "~/server/data-loader";
+import { isDevelopmentEnvironment } from "~/utils/app";
 import { dateWithoutTimezone } from "@diegofrayo/utils/dates";
 import { addLeftPadding } from "@diegofrayo/utils/strings";
 
@@ -12,11 +13,9 @@ export default BetsPage;
 // --- NEXT.JS FUNCTIONS ---
 
 export const getStaticProps: GetStaticProps<T_BetsPageProps> = async () => {
-	const monday = dayjs(dateWithoutTimezone(new Date("2024/05/06")));
-	console.log(monday);
-
+	const monday = dayjs(dateWithoutTimezone(new Date(2024, 4, 6)));
 	const dates = (
-		dateWithoutTimezone().getDay() <= 4
+		dateWithoutTimezone().getDay() >= 1 && dateWithoutTimezone().getDay() <= 4
 			? [
 					monday.toDate(),
 					monday.add(1, "day").toDate(),
@@ -29,22 +28,24 @@ export const getStaticProps: GetStaticProps<T_BetsPageProps> = async () => {
 					monday.add(6, "day").toDate(),
 			  ]
 	).map((date) => {
+		// TODO: Weird issue related to getDate()
 		return `${date.getFullYear()}-${addLeftPadding(date.getMonth() + 1)}-${addLeftPadding(
-			date.getDate(),
+			date.getDate() + (isDevelopmentEnvironment() ? 1 : 0),
 		)}`;
 	});
 	const data = {} as T_BetsPageProps["data"];
 
-	console.log(dateWithoutTimezone());
-	console.log(dateWithoutTimezone().getDay());
-	console.log("dates", [
-		monday.toDate(),
-		monday.add(1, "day").toDate(),
-		monday.add(2, "day").toDate(),
-		monday.add(3, "day").toDate(),
-	]);
-	console.log("dates-transformed", dates);
-	console.log("monday", monday);
+	// TODO: Remove these lines
+	// console.log("monday", monday.toDate());
+	// console.log("dateWithoutTimezone", dateWithoutTimezone());
+	// console.log("getDay", dateWithoutTimezone().getDay());
+	// console.log("dates", [
+	// 	monday.toDate(),
+	// 	monday.add(1, "day").toDate(),
+	// 	monday.add(2, "day").toDate(),
+	// 	monday.add(3, "day").toDate(),
+	// ]);
+	// console.log("dates-transformed", dates);
 
 	// eslint-disable-next-line no-loops/no-loops, no-restricted-syntax
 	for (const date of dates) {
