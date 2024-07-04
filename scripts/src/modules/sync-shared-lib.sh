0846a7b16161a51de3c6960c9adc2f4407bbd7ab
Diego Rayo <diegofrayo@gmail.com>
2024-07-04 18:25:18 -0500

    :bento: [update] assets

--- scripts/src/modules/sync-shared-lib.sh
+++ scripts/src/modules/sync-shared-lib.sh
@@ -47,3 +47,19 @@ cp ./src/lib/@diegofrayo/sort.ts "$bets_repo_dir"
 
 mkdir -p "$bets_repo_dir/types"
 cp ./src/lib/@diegofrayo/types/index.ts "$bets_repo_dir/types"
+
+# copy only required files to "rutas" repo
+rutas_repo_dir=../../demos/rutas/src/lib/@diegofrayo
+
+rm -r "$rutas_repo_dir"
+mkdir -p "$rutas_repo_dir/utils"
+cp ./src/lib/@diegofrayo/utils/arrays-and-objects.ts "$rutas_repo_dir/utils"
+cp ./src/lib/@diegofrayo/utils/dates.ts "$rutas_repo_dir/utils"
+cp ./src/lib/@diegofrayo/utils/files.ts "$rutas_repo_dir/utils"
+cp ./src/lib/@diegofrayo/utils/misc.ts "$rutas_repo_dir/utils"
+cp ./src/lib/@diegofrayo/utils/strings.ts "$rutas_repo_dir/utils"
+cp ./src/lib/@diegofrayo/v.ts "$rutas_repo_dir"
+cp ./src/lib/@diegofrayo/sort.ts "$rutas_repo_dir"
+
+mkdir -p "$rutas_repo_dir/types"
+cp ./src/lib/@diegofrayo/types/index.ts "$rutas_repo_dir/types"
