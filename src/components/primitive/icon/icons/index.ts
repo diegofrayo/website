b48490fb21d9f7ed78a8a6fb6dde02183b6ddaa7
Diego Rayo <diegofrayo@gmail.com>
2026-06-04 15:15:47 -0500

    chore: minor changes

--- src/components/primitive/icon/icons/index.ts
+++ src/components/primitive/icon/icons/index.ts
@@ -1,6 +1,20 @@
 import InlineIcons from "./inline-icons";
-import LibraryIcons from "./library-icons";
+import * as LibraryIconComponents from "./library-icons";
 import RemoteIcons from "./remote-icons";
+
+type LibraryIconsMap = {
+	[K in keyof typeof LibraryIconComponents]: {
+		icon: (typeof LibraryIconComponents)[K];
+		defaultProps: { className: string; color: string };
+	};
+};
+
+const LibraryIcons = Object.fromEntries(
+	Object.entries(LibraryIconComponents).map(([key, icon]) => [
+		key,
+		{ icon, defaultProps: { className: "", color: "" } },
+	]),
+) as LibraryIconsMap;
 
 const Icons = { ...InlineIcons, ...RemoteIcons, ...LibraryIcons };
 
--- src/components/primitive/icon/icons/library-icons.tsx
+++ src/components/primitive/icon/icons/library-icons.tsx
@@ -1,453 +1,56 @@
-/* eslint max-lines: 0 */
-
-import {
-	AppWindowIcon,
-	ArrowUpIcon,
-	BadgeCheckIcon,
-	BookTextIcon,
-	BracesIcon,
-	CalendarIcon,
-	CheckIcon,
-	ChevronLeftIcon,
-	ChevronRightIcon,
-	ChevronsDownIcon,
-	ChevronsUpIcon,
-	CircleUser,
-	CodeXmlIcon,
-	CopyIcon,
-	DollarSignIcon,
-	DownloadIcon,
-	ExpandIcon,
-	ExternalLinkIcon,
-	EyeIcon,
-	EyeOffIcon,
-	FileChartLineIcon,
-	FileTextIcon,
-	FilmIcon,
-	GlobeIcon,
-	GuitarIcon,
-	HeartIcon,
-	HourglassIcon,
-	IdCardIcon,
-	InfoIcon,
-	KeyIcon,
-	LanguagesIcon,
-	LinkIcon,
-	LogOutIcon,
-	MailsIcon,
-	MapPinIcon,
-	MenuIcon,
-	MessageSquareTextIcon,
-	PencilIcon,
-	PenToolIcon,
-	PhoneIcon,
-	RefreshCwIcon,
-	RssIcon,
-	SearchIcon,
-	ServerIcon,
-	SettingsIcon,
-	ShrinkIcon,
-	SlidersVerticalIcon,
-	StarIcon,
-	StoreIcon,
-	TagIcon,
-	TimerIcon,
-	TriangleAlertIcon,
-	TrophyIcon,
-	XIcon,
-	ZoomInIcon,
-	ZoomOutIcon,
-} from "lucide-react";
-
-const LibraryIcons = {
-	APP_WINDOW: {
-		defaultProps: {
-			className: "",
-			color: "",
-		},
-		icon: AppWindowIcon,
-	},
-	ARROW_UP: {
-		defaultProps: {
-			className: "",
-			color: "",
-		},
-		icon: ArrowUpIcon,
-	},
-	BADGE_CHECK: {
-		defaultProps: {
-			className: "",
-			color: "",
-		},
-		icon: BadgeCheckIcon,
-	},
-	BOOK_TEXT: {
-		defaultProps: {
-			className: "",
-		},
-		icon: BookTextIcon,
-	},
-	BRACES: {
-		defaultProps: {
-			className: "",
-			color: "",
-		},
-		icon: BracesIcon,
-	},
-	CALENDAR: {
-		defaultProps: {
-			className: "",
-			color: "",
-		},
-		icon: CalendarIcon,
-	},
-	CHECK: {
-		defaultProps: {
-			className: "",
-			color: "",
-		},
-		icon: CheckIcon,
-	},
-	CHEVRON_LEFT: {
-		defaultProps: {
-			className: "",
-			color: "",
-		},
-		icon: ChevronLeftIcon,
-	},
-	CHEVRON_RIGHT: {
-		defaultProps: {
-			className: "",
-			color: "",
-		},
-		icon: ChevronRightIcon,
-	},
-	CIRCLE_USER: {
-		defaultProps: {
-			className: "",
-			color: "",
-		},
-		icon: CircleUser,
-	},
-	CODE_XML: {
-		defaultProps: {
-			className: "",
-			color: "",
-		},
-		icon: CodeXmlIcon,
-	},
-	COPY: {
-		defaultProps: {
-			className: "",
-			color: "",
-		},
-		icon: CopyIcon,
-	},
-	CREVRONS_DOWN: {
-		defaultProps: {
-			className: "",
-			color: "",
-		},
-		icon: ChevronsDownIcon,
-	},
-	CREVRONS_UP: {
-		defaultProps: {
-			className: "",
-			color: "",
-		},
-		icon: ChevronsUpIcon,
-	},
-	DOLLAR_SIGN: {
-		defaultProps: {
-			className: "",
-			color: "",
-		},
-		icon: DollarSignIcon,
-	},
-	DOWNLOAD: {
-		defaultProps: {
-			className: "",
-			color: "",
-		},
-		icon: DownloadIcon,
-	},
-	EXPAND: {
-		defaultProps: {
-			className: "",
-			color: "",
-		},
-		icon: ExpandIcon,
-	},
-	EXTERNAL_LINK: {
-		defaultProps: {
-			className: "",
-			color: "",
-		},
-		icon: ExternalLinkIcon,
-	},
-	EYE: {
-		defaultProps: {
-			className: "",
-			color: "",
-		},
-		icon: EyeIcon,
-	},
-	EYE_OFF: {
-		defaultProps: {
-			className: "",
-			color: "",
-		},
-		icon: EyeOffIcon,
-	},
-	FILE_CHART_LINE: {
-		defaultProps: {
-			className: "",
-			color: "",
-		},
-		icon: FileChartLineIcon,
-	},
-	FILE_TEXT: {
-		defaultProps: {
-			className: "",
-			color: "",
-		},
-		icon: FileTextIcon,
-	},
-	FILM: {
-		defaultProps: {
-			className: "",
-			color: "",
-		},
-		icon: FilmIcon,
-	},
-	GLOBE: {
-		defaultProps: {
-			className: "",
-			color: "",
-		},
-		icon: GlobeIcon,
-	},
-	GUITAR: {
-		defaultProps: {
-			className: "",
-			color: "",
-		},
-		icon: GuitarIcon,
-	},
-	HEART: {
-		defaultProps: {
-			className: "",
-			color: "",
-		},
-		icon: HeartIcon,
-	},
-	HOURGLASS: {
-		defaultProps: {
-			className: "",
-			color: "",
-		},
-		icon: HourglassIcon,
-	},
-	ID_CARD: {
-		defaultProps: {
-			className: "",
-			color: "",
-		},
-		icon: IdCardIcon,
-	},
-	INFO: {
-		defaultProps: {
-			className: "",
-			color: "",
-		},
-		icon: InfoIcon,
-	},
-	KEY: {
-		defaultProps: {
-			className: "",
-			color: "",
-		},
-		icon: KeyIcon,
-	},
-	LANGUAGES: {
-		defaultProps: {
-			className: "",
-			color: "",
-		},
-		icon: LanguagesIcon,
-	},
-	LINK: {
-		defaultProps: {
-			className: "",
-			color: "",
-		},
-		icon: LinkIcon,
-	},
-	LOG_OUT: {
-		defaultProps: {
-			className: "",
-			color: "",
-		},
-		icon: LogOutIcon,
-	},
-	MAILS: {
-		defaultProps: {
-			className: "",
-			color: "",
-		},
-		icon: MailsIcon,
-	},
-	MAP_PIN: {
-		defaultProps: {
-			className: "",
-			color: "",
-		},
-		icon: MapPinIcon,
-	},
-	MENU: {
-		defaultProps: {
-			className: "",
-			color: "",
-		},
-		icon: MenuIcon,
-	},
-	MESSAGE_SQUARE_TEXT: {
-		defaultProps: {
-			className: "",
-			color: "",
-		},
-		icon: MessageSquareTextIcon,
-	},
-	PEN_TOOL: {
-		defaultProps: {
-			className: "",
-		},
-		icon: PenToolIcon,
-	},
-	PENCIL: {
-		defaultProps: {
-			className: "",
-		},
-		icon: PencilIcon,
-	},
-	PHONE: {
-		defaultProps: {
-			className: "",
-		},
-		icon: PhoneIcon,
-	},
-	REFRESH_CW: {
-		defaultProps: {
-			className: "",
-			color: "",
-		},
-		icon: RefreshCwIcon,
-	},
-	RSS: {
-		defaultProps: {
-			className: "",
-			color: "",
-		},
-		icon: RssIcon,
-	},
-	SEARCH: {
-		defaultProps: {
-			className: "",
-			color: "",
-		},
-		icon: SearchIcon,
-	},
-	SERVER: {
-		defaultProps: {
-			className: "",
-			color: "",
-		},
-		icon: ServerIcon,
-	},
-	SETTINGS: {
-		defaultProps: {
-			className: "",
-			color: "",
-		},
-		icon: SettingsIcon,
-	},
-	SHRINK: {
-		defaultProps: {
-			className: "",
-			color: "",
-		},
-		icon: ShrinkIcon,
-	},
-	SLIDERS_VERTICAL: {
-		defaultProps: {
-			className: "",
-			color: "",
-		},
-		icon: SlidersVerticalIcon,
-	},
-	STAR: {
-		defaultProps: {
-			className: "",
-			color: "",
-		},
-		icon: StarIcon,
-	},
-	STORE: {
-		defaultProps: {
-			className: "",
-			color: "",
-		},
-		icon: StoreIcon,
-	},
-	TAG: {
-		defaultProps: {
-			className: "",
-			color: "",
-		},
-		icon: TagIcon,
-	},
-	TIMER: {
-		defaultProps: {
-			className: "",
-			color: "",
-		},
-		icon: TimerIcon,
-	},
-	TRIANGLE_ALERT: {
-		defaultProps: {
-			className: "",
-			color: "",
-		},
-		icon: TriangleAlertIcon,
-	},
-	TROPHY: {
-		defaultProps: {
-			className: "",
-			color: "",
-		},
-		icon: TrophyIcon,
-	},
-	X: {
-		defaultProps: {
-			className: "",
-			color: "",
-		},
-		icon: XIcon,
-	},
-	ZOOM_IN: {
-		defaultProps: {
-			className: "",
-			color: "",
-		},
-		icon: ZoomInIcon,
-	},
-	ZOOM_OUT: {
-		color: "",
-		defaultProps: {
-			className: "",
-		},
-		icon: ZoomOutIcon,
-	},
-};
-
-export default LibraryIcons;
+export { AppWindowIcon as APP_WINDOW } from "lucide-react";
+export { ArrowUpIcon as ARROW_UP } from "lucide-react";
+export { BadgeCheckIcon as BADGE_CHECK } from "lucide-react";
+export { BookTextIcon as BOOK_TEXT } from "lucide-react";
+export { BracesIcon as BRACES } from "lucide-react";
+export { CalendarIcon as CALENDAR } from "lucide-react";
+export { CheckIcon as CHECK } from "lucide-react";
+export { ChevronLeftIcon as CHEVRON_LEFT } from "lucide-react";
+export { ChevronRightIcon as CHEVRON_RIGHT } from "lucide-react";
+export { CircleUser as CIRCLE_USER } from "lucide-react";
+export { CodeXmlIcon as CODE_XML } from "lucide-react";
+export { CopyIcon as COPY } from "lucide-react";
+export { ChevronsDownIcon as CREVRONS_DOWN } from "lucide-react";
+export { ChevronsUpIcon as CREVRONS_UP } from "lucide-react";
+export { DollarSignIcon as DOLLAR_SIGN } from "lucide-react";
+export { DownloadIcon as DOWNLOAD } from "lucide-react";
+export { ExpandIcon as EXPAND } from "lucide-react";
+export { ExternalLinkIcon as EXTERNAL_LINK } from "lucide-react";
+export { EyeIcon as EYE } from "lucide-react";
+export { EyeOffIcon as EYE_OFF } from "lucide-react";
+export { FileChartLineIcon as FILE_CHART_LINE } from "lucide-react";
+export { FileTextIcon as FILE_TEXT } from "lucide-react";
+export { FilmIcon as FILM } from "lucide-react";
+export { GlobeIcon as GLOBE } from "lucide-react";
+export { GuitarIcon as GUITAR } from "lucide-react";
+export { HeartIcon as HEART } from "lucide-react";
+export { HourglassIcon as HOURGLASS } from "lucide-react";
+export { IdCardIcon as ID_CARD } from "lucide-react";
+export { InfoIcon as INFO } from "lucide-react";
+export { KeyIcon as KEY } from "lucide-react";
+export { LanguagesIcon as LANGUAGES } from "lucide-react";
+export { LinkIcon as LINK } from "lucide-react";
+export { LogOutIcon as LOG_OUT } from "lucide-react";
+export { MailsIcon as MAILS } from "lucide-react";
+export { MapPinIcon as MAP_PIN } from "lucide-react";
+export { MenuIcon as MENU } from "lucide-react";
+export { MessageSquareTextIcon as MESSAGE_SQUARE_TEXT } from "lucide-react";
+export { PenToolIcon as PEN_TOOL } from "lucide-react";
+export { PencilIcon as PENCIL } from "lucide-react";
+export { PhoneIcon as PHONE } from "lucide-react";
+export { RefreshCwIcon as REFRESH_CW } from "lucide-react";
+export { RssIcon as RSS } from "lucide-react";
+export { SearchIcon as SEARCH } from "lucide-react";
+export { ServerIcon as SERVER } from "lucide-react";
+export { SettingsIcon as SETTINGS } from "lucide-react";
+export { ShrinkIcon as SHRINK } from "lucide-react";
+export { SlidersVerticalIcon as SLIDERS_VERTICAL } from "lucide-react";
+export { StarIcon as STAR } from "lucide-react";
+export { StoreIcon as STORE } from "lucide-react";
+export { TagIcon as TAG } from "lucide-react";
+export { TimerIcon as TIMER } from "lucide-react";
+export { TriangleAlertIcon as TRIANGLE_ALERT } from "lucide-react";
+export { TrophyIcon as TROPHY } from "lucide-react";
+export { XIcon as X } from "lucide-react";
+export { ZoomInIcon as ZOOM_IN } from "lucide-react";
+export { ZoomOutIcon as ZOOM_OUT } from "lucide-react";
